764ea3404744b918f8ac9042960f2b3a7aa0d661
thr828 <592040808@qq.com>
2020-09-20 23:13:57 +0800

    aspnet core web api

---
+++ Data/TodoContext .cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecastAPI.Models;
+
+namespace WeatherForecastAPI.Data
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> ToDoItems { get; set; }
+    }
+}
